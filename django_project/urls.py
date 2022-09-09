e4699d398ddef63ddd2d90c9872c51cbcacb9f8c
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-09-09 19:36:23 +0300

    MODIFY(delete and comment unnessary lined of code ))

--- django_project/urls.py
+++ django_project/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
+# if settings.DEBUG:
+#     import debug_toolbar
 
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+#     urlpatterns = [
+#         path("__debug__/", include(debug_toolbar.urls)),
+#     ] + urlpatterns
